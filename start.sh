63604e41bcf83af62ad1c3c0085f4bacda672f54
F0rc3Run <127685741+F0rc3Run@users.noreply.github.com>
2025-08-02 18:09:17 +0330

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "📦 Downloading xray-core..."
-curl -L -o xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip xray.zip
-chmod +x xray
-
-echo "🚀 Starting backend..."
-python3 main.py
